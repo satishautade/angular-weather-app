a5ad88ec91cbf63732191c23971f0adeb1915e8b
Satish Autade <satishautade@gmail.com>
2018-07-31 15:45:59 +1000

    Updated the layout and typography around app-current-weather component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:`
-  	<div style="text-align:center">
+  	<div>
   		<mat-toolbar color="primary">
   		<span>Welcome to Localcast Weather!</span>
   		</mat-toolbar>
-  		<div>Your city, your forecast, right now!</div>
-		<h2>Current Weather</h2>
-		<app-current-weather></app-current-weather>
+  		<div fxLayoutAlign="center">
+  		<div class="mat-caption">Your city, your forecast, right now!</div>
+  		</div>
+  		<div fxLayout="row">
+  		<div fxFlex></div>
+			<mat-card fxFlex="340px">
+			<mat-card-header>
+			<mat-card-title><div class="mat-title">Current Weather</div></mat-card-title>
+			</mat-card-header>
+			<mat-card-content>
+			<app-current-weather></app-current-weather>
+			</mat-card-content>
+			</mat-card>
+		<div fxFlex></div>
 	</div>
 	`
 })
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    MatButtonModule, MatToolbarModule, MatIconModule
+    MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule
   ],
   exports: [
-    MatButtonModule, MatToolbarModule, MatIconModule
+    MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule
   ]
 })
 export class MaterialModule { }
